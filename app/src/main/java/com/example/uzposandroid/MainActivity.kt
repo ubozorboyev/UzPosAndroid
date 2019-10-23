bf9689c2157f7667d3fba399966ebfe387429738
Ulugbek Bozorboyev <ulugbekbozorboyev990@gmail.com>
2019-10-23 20:47:52 +0500

    00000

--- app/src/main/java/com/example/uzposandroid/MainActivity.kt
+++ app/src/main/java/com/example/uzposandroid/MainActivity.kt
@@ -10,3 +10,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+/*
+ dp = px / (dpi / 160)
+.75  yoq ldpi( 120dpi)
+1.0  ustiga mdpi( 160dpi; asosiy)
+1.5  yoq hdpi( 240dpi)
+2.0  yoq xhdpi( 320dpi)
+3.0  yoq xxhdpi( 480dpi)
+4.0  yoq xxxhdpi( 640dpi)
+*/
